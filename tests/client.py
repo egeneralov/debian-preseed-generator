1f8d0a32a6ec8b79bfa209ce4b72cc95972ab3f3
Eduard Generalov <egeneralov@technotimes.org>
2019-02-04 14:34:24 +0300

    auto

--- tests/client.py
+++ tests/client.py
@@ -5,10 +5,16 @@ with open('test.json') as f:
   payload = f.read()
 
 
+# r = requests.post(
+#   'http://127.0.0.1:8080/build/',
+#   data = payload
+# )
+
 r = requests.post(
-  'http://127.0.0.1:8080/build/',
+  'https://debian-preseed-generator.herokuapp.com/build/',
   data = payload
 )
+
 
 print(r)
 print(dir(r))
